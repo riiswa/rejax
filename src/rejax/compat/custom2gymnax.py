b16e24ababfbf3b05039454323efd40c462db3e8
Waris Radji <wradji@sequel-flanders.lille.inria.fr>
2025-06-24 18:15:42 +0200

    Use pointax

--- src/rejax/compat/custom2gymnax.py
+++ src/rejax/compat/custom2gymnax.py
@@ -1,5 +1,5 @@
 from rejax.compat.envs.cartpole_swing_up import CartPoleSwingUp
-from rejax.compat.envs.point_maze import PointMazeEnv
+import pointax
 
 
 def create_custom(env_name, **kwargs):
@@ -7,17 +7,17 @@ def create_custom(env_name, **kwargs):
         env = CartPoleSwingUp(**kwargs)
         return env, env.default_params
     elif env_name == 'pointmaze-umaze-v0':
-        env = PointMazeEnv("UMaze")
+        env = pointax.make_umaze(**kwargs)
         return env, env.default_params
     elif env_name == 'pointmaze-open-v0':
-        env = PointMazeEnv("Open")
+        env = pointax.make_open(**kwargs)
         return env, env.default_params
     elif env_name == 'pointmaze-medium-v0':
-        env = PointMazeEnv("Medium")
+        env = pointax.make_medium(**kwargs)
         return env, env.default_params
     elif env_name == 'pointmaze-large-v0':
-        env = PointMazeEnv("Large")
+        env = pointax.make_large(**kwargs)
         return env, env.default_params
     elif env_name == 'pointmaze-giant-v0':
-        env = PointMazeEnv("Giant")
+        env = pointax.make_giant(**kwargs)
         return env, env.default_params
